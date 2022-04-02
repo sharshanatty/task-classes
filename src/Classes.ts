5414780e7bb5b178076c0c1d4274a3018bcf72a9
Natalia Sharshakova <sharshanatty@gmail.com>
2022-04-02 10:41:05 +0300

    Реализовала класс Счетчик

--- src/Classes.ts
+++ src/Classes.ts
@@ -1,8 +1,14 @@
-/*
-Реализуйте класс Counter:
-- Значение счётчика хранится в поле counter: number. Значение поля по умолчанию: 0
-- В конструктор можно передать параметр start: number - начальное значение счётчика.
-- Класс должен иметь метод increment, который увеличивает counter на 1 и возвращает результат
-- Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
- */
-export class Counter {}
+export class Counter {
+    counter: number = 0;
+    constructor(start: number = 0) {
+        this.counter = start;
+    }
+    increment(): number {
+        this.counter += 1;
+        return this.counter;
+    }
+    decrement(): number {
+        this.counter -= 1;
+        return this.counter;
+    }
+}
